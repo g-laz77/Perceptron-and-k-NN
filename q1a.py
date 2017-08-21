362651491d9ac7df1252f724f80fb5d5b10f7dae
g-laz77 <suhanprabhu@gmail.com>
2017-08-21 17:57:25 +0530

    finished writing the training part

--- q1a.py
+++ q1a.py
@@ -1,34 +1,40 @@
-import pandas as import pd
+import pandas as pd
 import numpy as np
 
 # TRAINING
 
 datafile = pd.read_csv("~/Documents/smai/assignment-1/dummy/datasets/q1/train.csv")
 train_data = datafile.iloc[0:,1:]
-test_class = datafile.iloc[0:,0]
+# print(train_data[0])
+train_class = datafile.iloc[0:,0]
 num_rows = train_data.shape[0]
 num_cols = train_data.shape[1]
-w = np.zeros(1,num_cols)
+w = np.ones((num_rows))
 
 def correct(row, weight_vec):
-    prod = np.dot(train_data[row],weight_vec)
-    if prod > (float)0:
+    # print(train_data[row])
+    prod = np.dot(weight_vec,train_data[row,:])
+    if prod > float(0):
         if test_class[row] == 1:
             err = 0
         else:
             err = 1
-            weight_vec = np.add(weight_vec,train_data[row])
-    else:
+            weight_vec = np.add(weight_vec,train_data[row,:])
+    elif prod < float(0):
         if test_class[row] == 0:
             err = 0
         else:
             err = 1
-            weight_vec = np.subtract(weight_vec,train_data[row])
+            weight_vec = np.subtract(weight_vec,train_data[row,:])
     return weight_vec, err
 
 while(1):
     count = 0
     for i in range(num_rows):
+        print(i)
         w, err = correct(i, w)
         count += err
-        
+    if count == 0:
+        print("Trained the weight vector")
+        break
+
