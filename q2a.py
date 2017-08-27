14e4d937de01b96a23f8511a5af5e8dbb8714e4c
g-laz77 <suhanprabhu@gmail.com>
2017-08-27 23:37:01 +0530

    finished q2a.py

--- q2a.py
+++ q2a.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import numpy as np
+import sys
 
 # TRAINING
-# datafile = pd.read_csv("~/Documents/smai/assignment-1/dummy/datasets/q2/train.csv")
-# train_data = datafile.iloc[0:,1:9].values
-# # print(train_data[0])
-# train_class = datafile.iloc[0:,10].values
-# #num_rows = train_data.shape[0]
-# #train_data = np.c_[np.ones((num_rows,1)), train_data]
-datafile = np.genfromtxt("/Users/sphinx/Documents/smai/assignment-1/dummy/datasets/q2/q2_breast_cancer.train.csv",delimiter=",")
-train_data = datafile[0:,1:10]
-train_class = datafile[0:,10]
-num_rows, num_cols = train_data.shape[:]
-print(train_data[0])
+train = sys.argv[1]
+test = sys.argv[2]
+datafile = pd.read_csv(train)
+train_data = datafile.iloc[0:,1:10].values
+# print(train_data[0])
+train_class = datafile.iloc[0:,10].values
 
+num_rows, num_cols = train_data.shape[:]
+for i in range(num_rows):
+    train_data[i][5] = int(train_data[i][5])
+train_data = np.c_[np.ones((num_rows,1)), train_data]
+num_rows, num_cols = train_data.shape[:]
 a = np.zeros((1, num_cols))
 print(num_rows,num_cols)
-b = 35
+b = 50
 
 for i in range(len(train_class)):       # Multiply by -1
     if train_class[i] == 2:
@@ -27,19 +28,15 @@ def relax(row, weight_vec):
     # print("ssup")
     prod = np.dot(weight_vec,train_data[row])
     err = 0
-    prod = 20
-    if prod < float(b):
+    if prod <= float(b):
         modulus = np.dot(train_data[row],train_data[row])
-        const = (b - prod) / modulus
-        temp = train_data[row]
-        for i in range(len(temp)):
-            temp[i] *= const
+        const = float(b - prod) / modulus
+        temp = np.multiply(const,train_data[row])
         weight_vec = np.add(weight_vec,temp)
         err = 1
-
     return weight_vec, err
 
-for i in range(200):        #number of epochs [Trial and error]
+for i in range(1000):        #number of epochs [Trial and error]
     #print("ssup")
     count = 0
     for i in range(num_rows):
@@ -54,30 +51,35 @@ for i in range(200):        #number of epochs [Trial and error]
 
 #Testing
 
-# def testFile(test_data, rows):
-#     count = 0
-#     for i in range(rows):
-#         prod = np.dot(a, test_data[i])
-#         if prod >= float(b):
-#             clas = 4
-#             if test_class[i] == clas:
-#                 count += 1     
-#         else:
-#             clas = 2
-#             if test_class[i] == clas:
-#                 count += 1
-#     return count
+def testFile(test_data, rows):
+    count = 0
+    for i in range(rows):
+        prod = np.dot(a, test_data[i])
+        if prod >= float(b):
+            clas = 4
+            if test_class[i] == clas:
+                count += 1     
+        else:
+            clas = 2
+            if test_class[i] == clas:
+                count += 1
+    return count
 
-# datafile = pd.read_csv("~/Documents/smai/assignment-1/dummy/datasets/q2/test.csv")
-# test_data = datafile.iloc[0:,1:9].values
-# num_rows, num_cols = test_data.shape[:]
-# print(num_rows,num_cols)
-# # print(train_data[0])
-# test_class = datafile.iloc[0:,10].values
-# num_rows = test_data.shape[0]
-# #test_data = np.c_[np.ones((num_rows,1)), test_data]
-# num_rows, num_cols = test_data.shape[:]
-# print(num_rows,num_cols)
+datafile = pd.read_csv(test)
+test_data = datafile.iloc[0:,1:10].values
+num_rows, num_cols = test_data.shape[:]
+print(num_rows,num_cols)
+num_rows, num_cols = test_data.shape[:]
+for i in range(num_rows):
+    test_data[i][5] = int(test_data[i][5])
+test_data = np.c_[np.ones((num_rows,1)), test_data]
+num_rows, num_cols = test_data.shape[:]
+# print(train_data[0])
+test_class = datafile.iloc[0:,10].values
+num_rows = test_data.shape[0]
+#test_data = np.c_[np.ones((num_rows,1)), test_data]
+num_rows, num_cols = test_data.shape[:]
+print(num_rows,num_cols)
 
-# count = testFile(test_data, num_rows)
-# print(count/float(num_rows))
+count = testFile(test_data, num_rows)
+print(count/float(num_rows))
--- q2b.py
+++ q2b.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import numpy as np
+import sys
 
 # TRAINING
-# datafile = pd.read_csv("~/Documents/smai/assignment-1/dummy/datasets/q2/train.csv")
-# train_data = datafile.iloc[0:,1:9].values
-# # print(train_data[0])
-# train_class = datafile.iloc[0:,10].values
-# #num_rows = train_data.shape[0]
-# #train_data = np.c_[np.ones((num_rows,1)), train_data]
-datafile = np.genfromtxt("/Users/sphinx/Documents/smai/assignment-1/dummy/datasets/q2/train.csv",delimiter=",")
-train_data = datafile[0:,1:10]
-train_class = datafile[0:,10]
+train = sys.argv[1]
+test = sys.argv[2]
+datafile = pd.read_csv(train)
+train_data = datafile.iloc[0:,1:10].values
+# print(train_data[0])
+train_class = datafile.iloc[0:,10].values
+
+num_rows, num_cols = train_data.shape[:]
+for i in range(num_rows):
+    train_data[i][5] = int(train_data[i][5])
+train_data = np.c_[np.ones((num_rows,1)), train_data]
 num_rows, num_cols = train_data.shape[:]
 a = np.zeros((1, num_cols))
 print(num_rows,num_cols)
-b = 35
+b = 50
 
 for i in range(len(train_class)):       # Multiply by -1
     if train_class[i] == 2:
@@ -25,15 +28,14 @@ def descent(weight_vec):            #gradient descent
     prod = np.dot(train_data,np.transpose(weight_vec))
     summ = np.zeros((1, num_cols))
     err = 0
+    print(prod)
     for i in range(len(prod)):
         if prod[i] < float(b):
             err += 1
             modulus = np.dot(train_data[i],train_data[i])
             const = (b - prod) / modulus
             temp = train_data[i]
-            for elem in range(len(temp)):
-                temp[elem] *= const
-            summ = np.add(summ,temp)
+            summ = np.add(summ,temp*const)
     weight_vec = np.add(weight_vec,summ)
     return weight_vec, err
 
@@ -61,7 +63,7 @@ def testFile(test_data, rows):
                 count += 1
     return count
 
-datafile = pd.read_csv("~/Documents/smai/assignment-1/dummy/datasets/q2/test.csv")
+datafile = pd.read_csv(test)
 test_data = datafile.iloc[0:,1:9].values
 num_rows, num_cols = test_data.shape[:]
 print(num_rows,num_cols)
