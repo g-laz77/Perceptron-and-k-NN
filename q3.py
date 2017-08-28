86fce8bbd272a66d2734365cb0f5dc8c3e156dd3
g-laz77 <suhanprabhu@gmail.com>
2017-08-28 23:52:24 +0530

    some more changes

--- q3.py
+++ q3.py
@@ -39,6 +39,7 @@ def train_split(index, value, dic, dataset, typ):
 			else:
 				right.append(row)
 		elif typ == "discrete":
+			# print("yo ",dic[row[index]],value)
 			if dic[row[index]] < value:
 				left.append(row)
 			else:
@@ -53,14 +54,16 @@ def train_split(index, value, dic, dataset, typ):
 # Select the best split point for a dataset
 def best_split(dataset):
 	classes = list(set(row[-4] for row in dataset))
-	best_index = 10000
-	best_value = 10000
+	best_index = 0
+	best_value = 0
 	least_entropy = 10000
 	best_groups = None
 	for index in range(len(dataset[0])):
+		print("---------------------------",index,"---------------------------")
 		if index == 6:
 			continue
-		data_val = datafile.iloc[1:,index].values
+		print(dataset)
+		data_val = dataset[0:,index]
 		distinct = list(set(dt for dt in data_val))
 		dict_dis = dict()
 		counter = 0
@@ -71,6 +74,7 @@ def best_split(dataset):
 			for i in range(len(distinct)):
 				groups = train_split(index, distinct[i], distinct, dataset, "binary")
 				entropy = entropy_cost(groups, classes)
+				print(entropy,index,distinct[i])
 				if entropy < least_entropy:
 					best_index = index
 					best_value = distinct[i]
@@ -81,23 +85,26 @@ def best_split(dataset):
 			for i in range(len(distinct)):
 				groups = train_split(index, dict_dis[distinct[i]], dict_dis, dataset, "discrete")
 				entropy = entropy_cost(groups, classes)
+				print(entropy,index,distinct[i],dict_dis[distinct[i]])				
 				if entropy < least_entropy:
 					best_index = index
 					best_value = distinct[i]
 					least_entropy = entropy
 					best_groups = groups
 
-		elif len(distinct) > 10:													#continuous data
+		else:													#continuous data
 			mx = np.amax(data_val)
 			mn = np.amin(data_val)
 			for val in range(100):
 				groups = train_split(index, float((mx-mn)*val)/100,distinct, dataset,"continuous")
 				entropy = entropy_cost(groups, classes)
+				print(entropy,index,float((mx-mn)*val)/100)				
 				if entropy < least_entropy:
 					best_index = index
 					best_value = float((mx-mn)*0.01*val)
 					least_entropy = entropy
 					best_groups = groups
+		print("Least Entropy:",least_entropy,index,best_value)
 
 	return {'index':best_index, 'value':best_value, 'groups':best_groups}
 
@@ -109,26 +116,33 @@ def node_terminal_eval(group):
 # Create child splits for a node or make terminal
 def split(node, max_depth, min_size, depth):
 	left, right = node['groups']
-	# del(node['groups'])
+	del(node['groups'])
     #to prevent overfitting
 	if depth >= max_depth:
 		node['l'] = node_terminal_eval(left) 
 		node['r'] = node_terminal_eval(right)
+		print("depth")
 		return
 	#to prevent overfitting
 	if not left or not right:
 		node['l'] = node['r'] = node_terminal_eval(left + right)
+		print("Nothing")
 		return
 
 	if len(left) <= min_size:
 		node['l'] = node_terminal_eval(left)
+		print("left_size")
 	else:
+		print("split_left")
+		print(left)
 		node['l'] = best_split(left)
 		split(node['l'], max_depth, min_size, depth+1)
 
 	if len(right) <= min_size:
+		print("right_size")
 		node['r'] = node_terminal_eval(right)
 	else:
+		print("split_right")
 		node['r'] = best_split(right)
 		split(node['r'], max_depth, min_size, depth+1)
  
@@ -140,7 +154,10 @@ def build_tree(train, max_depth, min_size):
 
 datafile = pd.read_csv(train)  #("~/Documents/smai/assignment-1/dummy/datasets/q3/train.csv")
 train_data = datafile.iloc[1:,0:].values
-train_class = datafile.iloc[1:, 6]
+train_class = datafile.iloc[1:,6]
 num_rows, num_cols = train_data.shape[:]
 
-build_tree(train_data,5,10)
+k = build_tree(train_data,5,50)
+# while(1):
+print(k)
+# 	k = k['r']
