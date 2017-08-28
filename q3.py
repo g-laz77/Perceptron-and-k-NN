4d0ede769411876039cc1aa8034b28eb4ca9b71a
g-laz77 <suhanprabhu@gmail.com>
2017-08-28 20:23:26 +0530

    fixed bugs

--- q3.py
+++ q3.py
@@ -3,12 +3,13 @@ import numpy as np
 import sys
 import math
 
-train = sys.argv(1)
-test = sys.argv(2)
+train = sys.argv[1]
+#test = sys.argv[2]
 
 mapp = list()
 for i in range(10):
 	mapp.append(i)
+
 # Calculate the entropy for a split dataset
 def entropy_cost(groups, classes):
 	n_instances = sum([len(group) for group in groups])
@@ -19,8 +20,12 @@ def entropy_cost(groups, classes):
 			continue
 		score = 0.0
 		for class_val in classes:
-			p = [row['left'] for row in group].count(class_val)
-			score -= p/size * math.log(p/size,2)
+			# for row in group:
+			# 	print(row['left'])
+			p = [row[6] for row in group].count(class_val)
+			#print(p,class_val,group)
+			if p != 0:
+				score -= p/size * math.log(p/size,2)
 		cost += score
 	return cost
 
@@ -28,7 +33,7 @@ def entropy_cost(groups, classes):
 def train_split(index, value, dic, dataset, typ):
 	left, right = list(), list()
 	for row in dataset:
-		if typ == "continuous"
+		if typ == "continuous":
 			if row[index] < value:
 				left.append(row)
 			else:
@@ -49,12 +54,12 @@ def train_split(index, value, dic, dataset, typ):
 def best_split(dataset):
 	classes = list(set(row[-4] for row in dataset))
 	best_index = 10000
-    best_value = 10000
-    least_entropy = 10000
-    best_groups = None
+	best_value = 10000
+	least_entropy = 10000
+	best_groups = None
 	for index in range(len(dataset[0])):
-        if index == 6:
-            continue
+		if index == 6:
+			continue
 		data_val = datafile.iloc[1:,index].values
 		distinct = list(set(dt for dt in data_val))
 		dict_dis = dict()
@@ -65,7 +70,7 @@ def best_split(dataset):
 		if len(distinct) == 2:
 			for i in range(len(distinct)):
 				groups = train_split(index, distinct[i], distinct, dataset, "binary")
-				entropy = entropy(groups, classes)
+				entropy = entropy_cost(groups, classes)
 				if entropy < least_entropy:
 					best_index = index
 					best_value = distinct[i]
@@ -75,19 +80,19 @@ def best_split(dataset):
 		elif len(distinct) == 3 or len(distinct) == 10:   # Non-continuous data
 			for i in range(len(distinct)):
 				groups = train_split(index, dict_dis[distinct[i]], dict_dis, dataset, "discrete")
-				entropy = entropy(groups, classes)
+				entropy = entropy_cost(groups, classes)
 				if entropy < least_entropy:
 					best_index = index
 					best_value = distinct[i]
 					least_entropy = entropy
 					best_groups = groups
 
-		elif distinct > 10:													#continuous data
+		elif len(distinct) > 10:													#continuous data
 			mx = np.amax(data_val)
 			mn = np.amin(data_val)
-			for val in range(len(100)):
+			for val in range(100):
 				groups = train_split(index, float((mx-mn)*val)/100,distinct, dataset,"continuous")
-				entropy = entropy(groups, classes)
+				entropy = entropy_cost(groups, classes)
 				if entropy < least_entropy:
 					best_index = index
 					best_value = float((mx-mn)*0.01*val)
@@ -99,22 +104,22 @@ def best_split(dataset):
 # Create a terminal node value
 def node_terminal_eval(group):
 	classes = [row[-4] for row in group]
-	return max(set(classses), key=classes.count)
+	return max(set(classes), key=classes.count)
 
 # Create child splits for a node or make terminal
 def split(node, max_depth, min_size, depth):
 	left, right = node['groups']
 	# del(node['groups'])
     #to prevent overfitting
-    if depth >= max_depth:
+	if depth >= max_depth:
 		node['l'] = node_terminal_eval(left) 
-        node['r'] = node_terminal_eval(right)
+		node['r'] = node_terminal_eval(right)
 		return
-    #to prevent overfitting
+	#to prevent overfitting
 	if not left or not right:
 		node['l'] = node['r'] = node_terminal_eval(left + right)
 		return
-	
+
 	if len(left) <= min_size:
 		node['l'] = node_terminal_eval(left)
 	else:
